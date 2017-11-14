b40578de49ff6bfbdbb37c2460cdb6289203d7a7
OVERWATCHER\oloap <paoloftuna@Googlemail.com>
2017-11-14 20:56:19 +0000

    fixed shaped not appearing

--- WVP/main.cpp
+++ WVP/main.cpp
@@ -98,7 +98,7 @@ struct Vertex	//Overloaded Vertex Structure
 D3D11_INPUT_ELEMENT_DESC layout[] =
 {
 	{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },  
-	{ "COLOUR", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0 },  
+	{ "COLOR", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0 },  
 };
 UINT numElements = ARRAYSIZE(layout);
 
@@ -497,7 +497,7 @@ int messageloop(){
 	while(true)
 	{
 		//checks there is a message
-		BOOL PeekMessageL( 
+		BOOL PeekMessageL(
 			LPMSG lpMsg,
 			HWND hWnd,
 			UINT wMsgFilterMin,
